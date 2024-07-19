f895a1dc13ad7583e6c2d4552fe71d1761b9b978
Abhishek34 <abhisheknehe@gmail.com>
2024-07-19 11:38:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-        strings: ["Full Stack Developer", "Youtuber", "Blogger"],
+        strings: ["Lead Full Stack Developer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
